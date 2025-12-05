add2670517f06fca100ecf080c8a46bca19fb561
Gustavo Oliveira <gustavoribeirogr253@gmail.com>
2025-12-05 14:11:36 -0300

    :bug: fix: removed unused element

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { Line, Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+// import { Line, Bar } from 'react-chartjs-2';
 import {
   Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend
 } from 'chart.js';
